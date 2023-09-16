5a9e6029b05f90ac556956d31a521a890ff3ea6c
VaishnaviGangwar <131783593+VaishnaviGangwar@users.noreply.github.com>
2023-09-16 13:02:04 +0530

    Update RS_streamlite_app.py

--- RS_streamlite_app.py
+++ RS_streamlite_app.py
@@ -55,7 +55,6 @@ if st.button('Show Recommendation'):
     with col2:
         st.text(recommended_movie_names[1])
         st.image(recommended_movie_posters[1])
-
     with col3:
         st.text(recommended_movie_names[2])
         st.image(recommended_movie_posters[2])
